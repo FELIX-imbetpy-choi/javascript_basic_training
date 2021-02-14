037c31bc5afff897dd9fd7eca81136e276274f55
Junyeong Choi <imbetpy2025@naver.com>
2021-02-14 14:25:10 +0900

    studing record on 20210214

---
+++ Part1/61-instanceof.js
@@ -0,0 +1,19 @@
+'user strict'
+
+
+// 'instanceof'로 클래스 확인하기
+// instanceof 연산자를 사용하면 객체가 특정 클래스에 속하는지 아닌지를 확인할 수 있습니다
+// instanceof 는 상속관계도 확인해줍니다
+
+
+// instanceof 연산자
+
+
+
+
+// 보너스: 타입 확인을 위한 Object.prototype.toString
+
+
+
+
+
